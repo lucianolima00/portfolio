4906d103ea678601b69b8aee4af50da5e376a571
Luciano Lima <lucianolima122123@gmail.com>
2021-08-17 11:58:51 -0300

    Button scroll and cards layout

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,4 +1,5 @@
 import './style.css';
+import CSS from 'csstype';
 
 import logo from '../../assets/lima-logo-black.png';
 import phone1 from '../../assets/iphone-portifolio-1.png';
@@ -17,6 +18,7 @@ import forwardButton from '../../assets/forward-button.png'
 import openOnGit from '../../assets/open-on-git.png'
 import cardOpen from '../../assets/card-open-button.png'
 import sendButton from '../../assets/send-button.png'
+import {useRef} from "react";
 
 var bgColors = { 
     "Purple": "#4F309B",
@@ -27,6 +29,12 @@ var bgColors = {
 };
 
 function Landing() {
+    const ref = useRef<HTMLDivElement>(document.createElement("div"));
+
+    const scroll = (scrollOffset = 0) => {
+        ref.current.scrollLeft += scrollOffset;
+    };
+
     return(
         <div id='page-landing'>
             <div id="background-middle">
@@ -48,8 +56,7 @@ function Landing() {
                         <img src={phone1} alt="" />
                     </div>
 
-                    <div id="social-background">
-                    </div>
+                    <div id="social-background"></div>
                     <div id="white-social-links">
                         <a href="https://github.com/lucianolima00" target="_blank" className="social-link">
                             <img src={githubWhite} alt=""/>
@@ -66,57 +73,120 @@ function Landing() {
                     </div>
                 </div>
 
-                {/* ABOUT */}
-
-                <div className="topic">
-                    <div className="topic-header">
-                        <span className="title">ABOUT</span>
-                    </div>
-                    <div className="topic-content">
-                        <div id="portfolio-phone-2">
-                            <img src={phone2} alt="" />
-                        </div>
-                        <div id="knownledge">
-                            <div id="knowledge-titles">
-                                <span>WEB DEVELOPER</span>
-                                <span>WEB DESIGNER</span>
-                                <span>MOBILE DEVELOPER</span>
-                            </div>
-                            <div id="knowledge-skills">
-                                <p>PHP</p>
-                                <p>RUBY</p>
-                                <p>FLUTTER</p>
-                                <p>PHOTOSHOP</p>
-                                <p>ILLUSTRATOR</p>
-                                <p>ADOBE XD</p>
-                                <p>REACT</p>
-                                <p>VUEJS</p>
-                                <p>JQUERY</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
                 {/* PROJECTS */}
 
-                <div className="topic">
+                <div className="topic project-topic">
                     <div className="topic-header">
                         <span className="title">PROJECTS</span>
                         <div id="interactive-buttons">
                             <div className="arrow-button">
-                                <img src={backButton} alt="" />
+                                <img src={backButton} alt="" onClick={() => scroll(-600)}/>
                             </div>
                             <div className="arrow-button">
-                                <img src={forwardButton} alt="" />
+                                <img src={forwardButton} alt="" onClick={() => scroll(600)}/>
                             </div>
                         </div>
                     </div>
-                    <div className="topic-content" id="project-content">
+                    <div id="project-content" ref={ref!}>
                         <div className="card">
                             <div className="card-body">
-                                <div className="card-picture">
-
+                                <div className="card-picture"></div>
+                                <div className="card-content">
+                                    <div className="card-tecnologies">
+                                        <div className="tech-dot" style={{backgroundColor:bgColors.Purple}}></div>
+                                        <div className="tech-name">
+                                            <p>PHP</p>
+                                        </div>
+                                        <div className="tech-dot" style={{backgroundColor:bgColors.Yellow}}></div>
+                                        <div className="tech-name">
+                                            <p>Javascript</p>
+                                        </div>
+                                    </div>
+                                    <div className="card-link">
+                                        <a href="https://github.com/lucianolima00">
+                                            <img src={openOnGit} alt="" />
+                                        </a>
+                                    </div>
                                 </div>
+                            </div>
+                            <div className="card-info">
+                                <div className="card-title">
+                                    <p>PORTFOLIO WEB</p>
+                                </div>
+                                <div className="card-button">
+                                    <a href="#">
+                                        <img src={cardOpen} alt="" />
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="card">
+                            <div className="card-body">
+                                <div className="card-picture"></div>
+                                <div className="card-content">
+                                    <div className="card-tecnologies">
+                                        <div className="tech-dot" style={{backgroundColor:bgColors.Purple}}></div>
+                                        <div className="tech-name">
+                                            <p>PHP</p>
+                                        </div>
+                                        <div className="tech-dot" style={{backgroundColor:bgColors.Yellow}}></div>
+                                        <div className="tech-name">
+                                            <p>Javascript</p>
+                                        </div>
+                                    </div>
+                                    <div className="card-link">
+                                        <a href="https://github.com/lucianolima00">
+                                            <img src={openOnGit} alt="" />
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="card-info">
+                                <div className="card-title">
+                                    <p>PORTFOLIO WEB</p>
+                                </div>
+                                <div className="card-button">
+                                    <a href="#">
+                                        <img src={cardOpen} alt="" />
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="card">
+                            <div className="card-body">
+                                <div className="card-picture"></div>
+                                <div className="card-content">
+                                    <div className="card-tecnologies">
+                                        <div className="tech-dot" style={{backgroundColor:bgColors.Purple}}></div>
+                                        <div className="tech-name">
+                                            <p>PHP</p>
+                                        </div>
+                                        <div className="tech-dot" style={{backgroundColor:bgColors.Yellow}}></div>
+                                        <div className="tech-name">
+                                            <p>Javascript</p>
+                                        </div>
+                                    </div>
+                                    <div className="card-link">
+                                        <a href="https://github.com/lucianolima00">
+                                            <img src={openOnGit} alt="" />
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="card-info">
+                                <div className="card-title">
+                                    <p>PORTFOLIO WEB</p>
+                                </div>
+                                <div className="card-button">
+                                    <a href="#">
+                                        <img src={cardOpen} alt="" />
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="card">
+                            <div className="card-body">
+                                <div className="card-picture"></div>
                                 <div className="card-content">
                                     <div className="card-tecnologies">
                                         <div className="tech-dot" style={{backgroundColor:bgColors.Purple}}></div>
@@ -149,34 +219,44 @@ function Landing() {
                     </div>
                 </div>
 
+                {/* ABOUT */}
+
+                <div className="topic">
+                    <div className="topic-header">
+                        <span className="title">ABOUT</span>
+                    </div>
+                    <div className="topic-content">
+                        <div id="portfolio-phone-2">
+                            <img src={phone2} alt="" />
+                        </div>
+                        <div id="knownledge">
+                            <div id="knowledge-titles">
+                                <span>WEB DEVELOPER</span>
+                                <span>WEB DESIGNER</span>
+                                <span>MOBILE DEVELOPER</span>
+                            </div>
+                            <div id="knowledge-skills">
+                                <p>PHP</p>
+                                <p>RUBY</p>
+                                <p>FLUTTER</p>
+                                <p>PHOTOSHOP</p>
+                                <p>ILLUSTRATOR</p>
+                                <p>ADOBE XD</p>
+                                <p>REACT</p>
+                                <p>NODEJS</p>
+                                <p>DEVOPS</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
                 {/* CONTACT */}
 
                 <div className="topic">
                     <div className="topic-header">
-                        <span className="title" id="contact-title">CONTACT</span>
+                        <span className="title">CONTACT</span>
                     </div>
                     <div className="topic-content">
-                        <div id="black-social-links">
-                            <a href="https://github.com/lucianolima00" target="_blank" className="social-link">
-                                <img src={githubBlack} alt=""/>
-                                <p>LUCIANOLIMA00</p>
-                            </a>
-                            <a href="https://www.behance.net/lucianolima00" target="_blank" className="social-link">
-                                <img src={behanceBlack} alt=""/>
-                                <p>LUCIANOLIMA00</p>
-                            </a>
-                            <a href="https://www.linkedin.com/in/lucianolima00" target="_blank" className="social-link">
-                                <img src={linkedinBlack} alt=""/>
-                                <p>LUCIANOLIMA00</p>
-                            </a>
-                            <a href="https://www.instagram.com/luciano.lima00" target="_blank" className="social-link">
-                                <img src={instaBlack} alt=""/>
-                                <p>LUCIANO.LIMA00</p>
-                            </a>
-                        </div>
-                        <div id="portfolio-phone-3">
-                            <img src={phone3} alt="" />
-                        </div>
                         <div className="contact-input">
                             <div className="contact-text">
                                 <textarea name="contact-textarea" id="contact-textarea" rows={15} cols={35} placeholder="MESSAGE"></textarea>
@@ -186,8 +266,32 @@ function Landing() {
                                 <img src={sendButton} alt="" />
                             </div>
                         </div>
+                        <div id="portfolio-phone-3">
+                            <img src={phone3} alt="" />
+                        </div>
+                        <div id="black-social-links">
+                            <a href="https://github.com/lucianolima00" target="_blank" className="social-link">
+                                <p>LUCIANOLIMA00</p>
+                                <img src={githubBlack} alt=""/>
+                            </a>
+                            <a href="https://www.behance.net/lucianolima00" target="_blank" className="social-link">
+                                <p>LUCIANOLIMA00</p>
+                                <img src={behanceBlack} alt=""/>
+                            </a>
+                            <a href="https://www.linkedin.com/in/lucianolima00" target="_blank" className="social-link">
+                                <p>LUCIANOLIMA00</p>
+                                <img src={linkedinBlack} alt=""/>
+                            </a>
+                            <a href="https://www.instagram.com/luciano.lima00" target="_blank" className="social-link">
+                                <p>LUCIANO.LIMA00</p>
+                                <img src={instaBlack} alt=""/>
+                            </a>
+                        </div>
                     </div>
                 </div>
+            </div>
+            <div id="background-bottom-container">
+                <div id="background-bottom"></div>
             </div>
         </div>
     );
